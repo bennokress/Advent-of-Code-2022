4dffeae80f6b71c6acb622a0608d8c77eb2c5440
BK | Mobile Apps <developer+git-commit@bennokress.de>
2022-12-05 12:54:36 +0100

    [Day 05] Solution

--- Sources/AdventOfCode2022/Day 05.swift
+++ Sources/AdventOfCode2022/Day 05.swift
@@ -7,21 +7,33 @@ import Foundation
 
 public class Solver05 {
     
+    /// Crate Stacks State where first element is the one on top
+    private var stacksAfterSinglesOperation: [String] = []
+    private var stacksAfterMultiplesOperation: [String] = []
+    
     init() { }
     
-    /// Instructions what to do with the input
     func parseStuff(from input: String) {
-        print(input)
+        let inputEntries = input.split(separator: "\n\n")
+        let instructions = inputEntries.last!.components(separatedBy: .newlines).map { StackOperationInstruction($0) }
+        
+        var singlesStacks = CrateStacks(from: String(inputEntries.first!))
+        for instruction in instructions { instruction.useCrateMover9000(on: &singlesStacks) }
+        self.stacksAfterSinglesOperation = singlesStacks.currentState
+        
+        var multiplesStacks = CrateStacks(from: String(inputEntries.first!))
+        for instruction in instructions { instruction.useCrateMover9001(on: &multiplesStacks) }
+        self.stacksAfterMultiplesOperation = multiplesStacks.currentState
     }
     
-    /// Answer 1 Description
-    var answer1: Int {
-        0
+    /// The top crate of each stack after using the instructions to move one crate at a time (CrateMover 9000)
+    var answer1: String {
+        String(stacksAfterSinglesOperation.map(\.first!))
     }
     
-    /// Answer 2 Description
-    var answer2: Int {
-        0
+    /// The top crate of each stack after using the instructions to move all crates from a single stack at the same time (CrateMover 9001)
+    var answer2: String {
+        String(stacksAfterMultiplesOperation.map(\.first!))
     }
     
 }
@@ -30,9 +42,9 @@ extension Solver05: Solver {
     
     public func solve(using input: String) { parseStuff(from: input) }
     
-    public var solutionPart1: String { String(answer1) }
+    public var solutionPart1: String { answer1 }
     
-    public var solutionPart2: String { String(answer2) }
+    public var solutionPart2: String { answer2 }
     
 }
 
@@ -53,4 +65,59 @@ extension Solver05: CustomStringConvertible {
 
 // MARK: - Supporting Types
 
-// Add whatever you need
+struct StackOperationInstruction {
+    
+    let amount: Int
+    let originStackIndex: Int
+    let destinationStackIndex: Int
+    
+    init(_ input: String) {
+        let inputWordsAndNumbers = input.components(separatedBy: .whitespaces)
+        let numbersFromInput = inputWordsAndNumbers.compactMap { Int(String($0)) }
+        self.amount = numbersFromInput[0]
+        self.originStackIndex = numbersFromInput[1] - 1 // lower index by 1
+        self.destinationStackIndex = numbersFromInput[2] - 1 // lower index by 1
+    }
+    
+    func useCrateMover9000(on stacks: inout CrateStacks) {
+        stacks.moveSingles(amount, from: originStackIndex, to: destinationStackIndex)
+    }
+    
+    func useCrateMover9001(on stacks: inout CrateStacks) {
+        stacks.moveMultiples(amount, from: originStackIndex, to: destinationStackIndex)
+    }
+    
+}
+
+struct CrateStacks {
+    
+    private(set) var currentState: [String]
+    
+    init(from input: String) {
+        var inputEntries: [String] = input.components(separatedBy: .newlines)
+        let stackAmount = inputEntries.removeLast().compactMap { Int(String($0)) }.max()!
+        var stacks: [String] = Array(repeating: "", count: stackAmount)
+        for inputEntry in inputEntries {
+            for (index, stackElement) in inputEntry.chunks(ofCount: 4).enumerated() {
+                if let crate = stackElement.first(where: \.isLetter) {
+                    stacks[index].append(crate)
+                }
+            }
+        }
+        self.currentState = stacks
+    }
+    
+    mutating func moveSingles(_ amount: Int, from originStackIndex: Int, to destinationStackIndex: Int) {
+        for _ in 1...amount {
+            let movingCrate = currentState[originStackIndex].removeFirst()
+            currentState[destinationStackIndex] = "\(movingCrate)\(currentState[destinationStackIndex])"
+        }
+    }
+    
+    mutating func moveMultiples(_ amount: Int, from originStackIndex: Int, to destinationStackIndex: Int) {
+        let movingCrates = currentState[originStackIndex].chunks(ofCount: amount).first!
+        currentState[originStackIndex].removeFirst(amount)
+        currentState[destinationStackIndex] = "\(movingCrates)\(currentState[destinationStackIndex])"
+    }
+    
+}
--- Tests/AdventOfCode2022Tests/Day05Tests.swift
+++ Tests/AdventOfCode2022Tests/Day05Tests.swift
@@ -4,15 +4,23 @@ import XCTest
 final class Day05Tests: XCTestCase {
     
     let input = """
-        Paste example input here ...
+            [D]    
+        [N] [C]    
+        [Z] [M] [P]
+        1   2   3 
+        
+        move 1 from 2 to 1
+        move 3 from 1 to 3
+        move 2 from 2 to 1
+        move 1 from 1 to 2
         """
     
     func testExample() throws {
         let solver = Solver05()
         solver.solve(using: input)
         
-        XCTAssertEqual(solver.answer1, 0)
-        XCTAssertEqual(solver.answer2, 0)
+        XCTAssertEqual(solver.answer1, "CMZ")
+        XCTAssertEqual(solver.answer2, "MCD")
     }
     
 }
