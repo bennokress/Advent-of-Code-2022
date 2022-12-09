5fd3890f786cc4a6994fe89ebb68f026640f5896
BK | Mobile Apps <developer+git-commit@bennokress.de>
2022-12-09 14:10:29 +0100

    [Day 09] Solution

--- Sources/AdventOfCode2022/Day 09.swift
+++ Sources/AdventOfCode2022/Day 09.swift
@@ -7,21 +7,52 @@ import Foundation
 
 public class Solver09 {
     
+    var visitedTailPositions: Set<RopePosition> = [RopePosition(x: 0, y: 0)]
+    var visitedLongTailPositions: Set<RopePosition> = [RopePosition(x: 0, y: 0)]
+    var head = RopePosition(x: 0, y: 0)
+    var tail1 = RopePosition(x: 0, y: 0)
+    var tail2 = RopePosition(x: 0, y: 0)
+    var tail3 = RopePosition(x: 0, y: 0)
+    var tail4 = RopePosition(x: 0, y: 0)
+    var tail5 = RopePosition(x: 0, y: 0)
+    var tail6 = RopePosition(x: 0, y: 0)
+    var tail7 = RopePosition(x: 0, y: 0)
+    var tail8 = RopePosition(x: 0, y: 0)
+    var tail9 = RopePosition(x: 0, y: 0)
+    
     init() { }
     
-    /// Instructions what to do with the input
     func parseStuff(from input: String) {
-        print(input)
+        let inputEntries = input.components(separatedBy: .newlines)
+        for inputEntry in inputEntries {
+            let instruction = inputEntry.components(separatedBy: .whitespaces)
+            let repetitions = Int(instruction.last!)!
+            let direction = RopePosition.MoveDirection(rawValue: instruction.first!)!
+            for _ in 1...repetitions {
+                self.head = head.nextPosition(moving: direction)
+                self.tail1 = tail1.nextPosition(dependingOn: head)
+                self.tail2 = tail2.nextPosition(dependingOn: tail1)
+                self.tail3 = tail3.nextPosition(dependingOn: tail2)
+                self.tail4 = tail4.nextPosition(dependingOn: tail3)
+                self.tail5 = tail5.nextPosition(dependingOn: tail4)
+                self.tail6 = tail6.nextPosition(dependingOn: tail5)
+                self.tail7 = tail7.nextPosition(dependingOn: tail6)
+                self.tail8 = tail8.nextPosition(dependingOn: tail7)
+                self.tail9 = tail9.nextPosition(dependingOn: tail8)
+                self.visitedTailPositions.insert(tail1)
+                self.visitedLongTailPositions.insert(tail9)
+            }
+        }
     }
     
-    /// Answer 1 Description
+    /// How many positions was the tail of the short rope on at least once?
     var answer1: Int {
-        0
+        visitedTailPositions.count
     }
     
-    /// Answer 2 Description
+    /// How many positions was the tail of the long rope on at least once?
     var answer2: Int {
-        0
+        visitedLongTailPositions.count
     }
     
 }
@@ -53,4 +84,47 @@ extension Solver09: CustomStringConvertible {
 
 // MARK: - Supporting Types
 
-// Add whatever you need
+struct RopePosition: Equatable, Identifiable, Hashable {
+    
+    let x, y: Int
+    
+    var id: String { description }
+    var description: String { "[\(x),\(y)]" }
+    
+    func nextPosition(moving direction: MoveDirection) -> RopePosition {
+        switch direction {
+        case .up: return RopePosition(x: x, y: y + 1)
+        case .down: return RopePosition(x: x, y: y - 1)
+        case .left: return RopePosition(x: x - 1, y: y)
+        case .right: return RopePosition(x: x + 1, y: y)
+        }
+    }
+    
+    func nextPosition(dependingOn head: RopePosition) -> RopePosition {
+        guard !isAdjacent(to: head) else { return self }
+        let distance = (x: horizontalDistance(to: head), y: verticalDistance(to: head))
+        let nextX = (abs(distance.x) < 1) ? x : (x + distance.x.signum())
+        let nextY = (abs(distance.y) < 1) ? y : (y + distance.y.signum())
+        return RopePosition(x: nextX, y: nextY)
+    }
+    
+    private func isAdjacent(to otherPosition: RopePosition) -> Bool {
+        abs(horizontalDistance(to: otherPosition)) <= 1 && abs(verticalDistance(to: otherPosition)) <= 1
+    }
+    
+    private func horizontalDistance(to otherPosition: RopePosition) -> Int {
+        otherPosition.x - x
+    }
+    
+    private func verticalDistance(to otherPosition: RopePosition) -> Int {
+        otherPosition.y - y
+    }
+    
+    enum MoveDirection: String {
+        case up = "U"
+        case down = "D"
+        case left = "L"
+        case right = "R"
+    }
+    
+}
--- Tests/AdventOfCode2022Tests/Day09Tests.swift
+++ Tests/AdventOfCode2022Tests/Day09Tests.swift
@@ -1,18 +1,47 @@
 import XCTest
 @testable import AdventOfCode2022
 
-final class Day09Tests: XCTestCase {
+final class Day09ATests: XCTestCase {
     
     let input = """
-        Paste example input here ...
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
         """
     
     func testExample() throws {
         let solver = Solver09()
         solver.solve(using: input)
         
-        XCTAssertEqual(solver.answer1, 0)
-        XCTAssertEqual(solver.answer2, 0)
+        XCTAssertEqual(solver.answer1, 13)
+        XCTAssertEqual(solver.answer2, 1)
+    }
+    
+}
+
+final class Day09BTests: XCTestCase {
+    
+    let input = """
+        R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20
+        """
+    
+    func testExample() throws {
+        let solver = Solver09()
+        solver.solve(using: input)
+        
+        XCTAssertEqual(solver.answer2, 36)
     }
     
 }
